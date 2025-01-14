21ddda9f4c7a56e9b1c4e45d3ef41e3a26fe1654
CoffeeWarka <erleronphoenix@gmail.com>
2025-01-14 10:51:35 +0300

    Update admin.py

--- tester/quiz/admin.py
+++ tester/quiz/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-
 from .models import Question, Answer
 
-class AllAnswer(admin.TabularInLine):
+class AllAnswer(admin.TabularInline):
   model = Answer
   extra = 4
 
 class QuestionAdmin(admin.ModelAdmin):
   fieldsets = [(None, {'fields':['question_text']})]
-  allanswer = [AllAnswer]
+  inlines = [AllAnswer]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
